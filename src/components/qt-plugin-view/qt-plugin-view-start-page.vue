56c8e83c69fd07e7892d1dc96d79f1fdde039864
liulipeng <liulipeng@huan.tv>
2024-06-14 16:57:37 +0800

    feat: plugin-item && plugin-section

--- src/components/qt-plugin-view/qt-plugin-view-start-page.vue
+++ src/components/qt-plugin-view/qt-plugin-view-start-page.vue
@@ -3,18 +3,38 @@
     <s-title-view class="es-sdk-content-title-css" :text="this.$options.name"/>
     <div class="es-sdk-content-divider-css"/>
     <qt-view class="qt-text-start-page">
-      <qt-plugin-view pluginKey="plugin-hello" class="qt-plugin-view-css"/>
+      <qt-plugin-view
+          ref="pluginView"
+          pluginKey="plugin-hello"
+          class="qt-plugin-view-css"/>
     </qt-view>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "@vue/runtime-core";
+import {defineComponent, ref} from "@vue/runtime-core";
+import {QTIPluginView} from "@quicktvui/quicktvui3";
+import {useESToast} from "@extscreen/es3-core";
 
 export default defineComponent({
   name: '使用初探',
   setup(props, context) {
-    return {}
+    const pluginView = ref<QTIPluginView>();
+    const toast = useESToast()
+
+    function onESCreate(params) {
+      pluginView.value?.dispatchFunction('changeAlpha', [0.5])
+          .then((ret) => {
+            toast.showToast(JSON.stringify(ret))
+          }, error => {
+            toast.showToast(error)
+          })
+    }
+
+    return {
+      onESCreate,
+      pluginView
+    }
   },
 });
 </script>
