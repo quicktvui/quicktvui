1cf383ea74171edaa4da31513332c76d06acc79e
xingrugeng <xrg3565119@>
2024-09-03 18:20:07 +0800

    feat:优化加载

--- packages/src/tab/index.vue
+++ packages/src/tab/index.vue
@@ -36,7 +36,7 @@
       <slot name="tab-item"/>
     </qt-nav-bar>
 
-    <recycler-view-pager v-if="loadContent"
+    <recycler-view-pager v-if="contentRenderingComponents"
         :nextFocusName="contentNextFocus"
         ref="viewPager"
         :class="tabPageClass"
@@ -290,6 +290,10 @@ export default defineComponent({
       type:Number,
       default:0
     },
+    contentRenderingComponents:{
+      type:Boolean,
+      default:true
+    },
     qtTabSectionEnable:{
       type:Object,
       default:()=>{
@@ -333,10 +337,8 @@ export default defineComponent({
     let lastTabPageIndex: number = -1
     let notifySectionAttachTimer: any = -1
 
-    let loadContent = ref(false)
     //--------------------------------------------------------
     function initTab(tab: QTTab): void {
-      loadContent.value = true
       let data: ESTab = {
         defaultIndex: tab.defaultIndex,
         focusIndex: tab.defaultFocusIndex,
@@ -1021,7 +1023,6 @@ export default defineComponent({
       onTabClick,
       getCurrentPageIndex,
       getCurrentTabIndex,
-      loadContent,
       onPluginLoadSuccess,
       onPluginLoadError,
       getDataManager(){
