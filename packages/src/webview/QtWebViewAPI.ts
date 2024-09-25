5e57aa50c2306695712e7f905f8ae9ee9eed8f84
liulipeng <liulipeng@huan.tv>
2024-09-25 15:42:05 +0800

    全局变量qt

--- packages/src/webview/QtWebViewAPI.ts
+++ packages/src/webview/QtWebViewAPI.ts
@@ -3,6 +3,7 @@ import {Native} from "@extscreen/es3-vue";
 import {isRef, Ref} from "@vue/reactivity";
 import {QTIWebView} from "./QTIWebView";
 import {QT_CALL_UI_FUNCTION, QT_API_MODULE} from "../qt/QtAPI";
+import {isString} from "../utils/type";
 
 export interface QtWebViewAPI extends QtViewAPI {
   loadUrl(sid: string | Ref<QTIWebView | undefined>, url: string): void
@@ -11,7 +12,7 @@ export interface QtWebViewAPI extends QtViewAPI {
 export function createQtWebViewAPI(viewAPI: QtViewAPI): QtWebViewAPI {
 
   function loadUrl(instance: string | Ref<QTIWebView | undefined>, url: string): void {
-    if (instance instanceof String) {
+    if (isString(instance)) {
       console.log("---------loadUrl----------String------>>>>>")
       Native.callNative(QT_API_MODULE, QT_CALL_UI_FUNCTION, instance, 'loadUrl', [url]);
     } else if (isRef(instance)) {
