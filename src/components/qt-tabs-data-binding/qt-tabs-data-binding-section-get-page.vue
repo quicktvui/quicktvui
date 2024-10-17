f7dc8f0629a76c60993930e42277c955d94e9449
liulipeng <liulipeng@huan.tv>
2024-10-17 17:31:26 +0800

    tabs数据绑定Demo

--- src/components/qt-tabs-data-binding/qt-tabs-data-binding-section-get-page.vue
+++ src/components/qt-tabs-data-binding/qt-tabs-data-binding-section-get-page.vue
@@ -7,6 +7,7 @@
     </div>
     <qt-tabs
       ref="tabRef"
+      :datas="tabData"
       @onTabPageLoadData="onTabPageLoadData"
       class="qt-tabs-css">
       <template v-slot:waterfall-item>
@@ -21,21 +22,24 @@
 import {defineComponent} from "@vue/runtime-core";
 import {ref} from "vue";
 import {
-  QTITab, QTTabPageData, QTWaterfall, QTWaterfallSection, QTTabItem, QTTab
+  QTITab, QTTabPageData, QTWaterfall, QTWaterfallSection, QTTabItem, QTTab, qtTabsRef
 } from "@quicktvui/quicktvui3";
 import {generatorAppWaterfallSection} from "../__mocks__/app";
 import app_list_item from './item/app-list-item'
 import {useESLog} from "@extscreen/es3-core";
+import {buildTabItemList} from "../__mocks__/tab";
 
 const TAG = 'TabWaterfallTAG'
 
 export default defineComponent({
-  name: 'DataBinding 查询Section',
+  name: 'DataBinding 查询Section ?',
   components: {
     'app-list-item': app_list_item
   },
   setup(props, context) {
     const tabRef = ref<QTITab>()
+    const tabData = qtTabsRef()
+
     const log = useESLog()
 
     function onGetButtonClicked() {
@@ -44,38 +48,7 @@ export default defineComponent({
     }
 
     function onESCreate() {
-
-      //tab item list
-      const tabItemList: Array<QTTabItem> = []
-
-      for (let i = 0; i < 15; i++) {
-        let tabItem: QTTabItem = {
-          _id: '' + i,
-          type: 20000,
-          text: 'Tab:' + i,
-          titleSize: 20,
-          decoration: {
-            left: 40,
-            right: 20,
-          }
-        }
-        tabItemList.push(tabItem)
-      }
-
-      //tab
-      const tab: QTTab = {
-        defaultFocusIndex: 0,
-        defaultIndex: 0,
-        itemList: tabItemList
-      }
-      tabRef.value?.initTab(tab)
-
-
-      let waterfallData: QTWaterfall = {
-        width: 1920,
-        height: 1080
-      }
-      tabRef.value?.initPage(waterfallData)
+      tabData.value = buildTabItemList()
     }
 
     let pageIndexLast = -1
@@ -92,20 +65,12 @@ export default defineComponent({
       pageIndexLast = pageIndex
 
       let section: QTWaterfallSection = generatorAppWaterfallSection('0', "应用")
-
-      let sectionList: Array<QTWaterfallSection> = [
-        section,
-      ]
-
-      const tabPage: QTTabPageData = {
-        useDiff: useDiff,
-        data: sectionList
-      }
-      tabRef.value?.setPageData(pageIndex, tabPage)
+      tabData.value[pageIndex].sections.push(section)
     }
 
     return {
       tabRef,
+      tabData,
       onESCreate,
       onTabPageLoadData,
       onGetButtonClicked
--- src/components/qt-tabs-data-binding/qt-tabs-data-binding-section-update-page.vue
+++ src/components/qt-tabs-data-binding/qt-tabs-data-binding-section-update-page.vue
@@ -6,9 +6,10 @@
       <s-text-button text="更新Section" @onButtonClicked="onButtonClicked"/>
     </div>
     <qt-tabs
-      ref="tabRef"
-      @onTabPageLoadData="onTabPageLoadData"
-      class="qt-tabs-css">
+        ref="tabRef"
+        :datas="tabData"
+        @onTabPageLoadData="onTabPageLoadData"
+        class="qt-tabs-css">
       <template v-slot:waterfall-item>
         <app-list-item :type="1"/>
       </template>
@@ -21,7 +22,7 @@
 import {defineComponent} from "@vue/runtime-core";
 import {ref} from "vue";
 import {
-  QTITab, QTTabPageData, QTWaterfall, QTWaterfallSection, QTTabItem, QTTab
+  QTITab, QTTabPageData, QTWaterfall, QTWaterfallSection, QTTabItem, QTTab, qtTabsRef
 } from "@quicktvui/quicktvui3";
 import {
   generatorAppQuestionWaterfallItemList,
@@ -29,55 +30,24 @@ import {
   generatorWaterfallSection
 } from "../__mocks__/app";
 import app_list_item from './item/app-list-item'
+import {buildTabItemList} from "../__mocks__/tab";
 
 export default defineComponent({
-  name: 'DataBinding 更新Section',
+  name: 'DataBinding 更新Section ?',
   components: {
     'app-list-item': app_list_item
   },
   setup(props, context) {
-    const tabRef = ref<QTITab>()
+    const tabData = qtTabsRef()
 
     function onButtonClicked() {
-
       const itemList = generatorAppQuestionWaterfallItemList('0', 5)
       let section: QTWaterfallSection = generatorWaterfallSection('0', "应用更新", itemList)
-      tabRef.value?.updatePageSection(0, 0, section)
+      tabData.value[0].sections[0] = section
     }
 
     function onESCreate() {
-
-      //tab item list
-      const tabItemList: Array<QTTabItem> = []
-
-      for (let i = 0; i < 15; i++) {
-        let tabItem: QTTabItem = {
-          _id: '' + i,
-          type: 20000,
-          text: 'Tab:' + i,
-          titleSize: 20,
-          decoration: {
-            left: 40,
-            right: 20,
-          }
-        }
-        tabItemList.push(tabItem)
-      }
-
-      //tab
-      const tab: QTTab = {
-        defaultFocusIndex: 0,
-        defaultIndex: 0,
-        itemList: tabItemList
-      }
-      tabRef.value?.initTab(tab)
-
-
-      let waterfallData: QTWaterfall = {
-        width: 1920,
-        height: 1080
-      }
-      tabRef.value?.initPage(waterfallData)
+      tabData.value = buildTabItemList()
     }
 
     let pageIndexLast = -1
@@ -85,8 +55,8 @@ export default defineComponent({
     //-----------------------------------------------------
     function onTabPageLoadData(pageIndex: number, pageNo: number, useDiff: boolean): void {
       console.log('---------loadPageData---------->>>' +
-        '  pageIndex:' + pageIndex +
-        '  useDiff:' + useDiff
+          '  pageIndex:' + pageIndex +
+          '  useDiff:' + useDiff
       )
       if (pageIndexLast === pageIndex) {
         return
@@ -99,16 +69,11 @@ export default defineComponent({
       let sectionList: Array<QTWaterfallSection> = [
         section_1, section_2
       ]
-
-      const tabPage: QTTabPageData = {
-        useDiff: useDiff,
-        data: sectionList
-      }
-      tabRef.value?.setPageData(pageIndex, tabPage)
+      tabData.value[pageIndex].sections.push(...sectionList)
     }
 
     return {
-      tabRef,
+      tabData,
       onESCreate,
       onTabPageLoadData,
       onButtonClicked,
