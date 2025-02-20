f3606eee3cf50405518b3cb4ffb94cb49d1af1c4
liulipeng <liulipeng@huan.tv>
2025-02-20 18:29:44 +0800

    qt-ul demo

--- src/components/qt-ul/index.js
+++ src/components/qt-ul/index.js
@@ -2,7 +2,7 @@ import qt_ul_basic_page from './qt-ul-basic-page';
 import qt_ul_item_image_page from './qt-ul-item-image-page';
 import qt_ul_item_text_page from './qt-ul-item-text-page';
 import qt_ul_item_focusable_text_page from './qt-ul-item-focusable-text-page';
-
+import qt_ul_item_type_page from './qt-ul-item-type-page';
 
 const QTULPageList = {
     qt_ul_basic_page: {
@@ -20,6 +20,10 @@ const QTULPageList = {
     qt_ul_item_focusable_text_page: {
         name: '文字焦点框',
         component: qt_ul_item_focusable_text_page,
+    },
+    qt_ul_item_type_page: {
+        name: '类型',
+        component: qt_ul_item_type_page,
     }
 };
 export default QTULPageList;
--- src/components/qt-ul/qt-ul-item-type-page.vue
+++ src/components/qt-ul/qt-ul-item-type-page.vue
@@ -9,11 +9,19 @@
           :spanCount="2"
           :enablePlaceholder="false">
         <template #item="{item}">
+
           <qt-ul-item-text
               :text="item.text"
               v-if="item.type == 3"
               :focusable="true"
               :enableFocusBorder="true"/>
+
+          <qt-ul-item-image
+              :src="item.url"
+              v-if="item.type == 2"
+              :focusable="true"
+              :enableFocusBorder="true"/>
+
         </template>
       </qt-ul>
     </qt-view>
@@ -23,24 +31,36 @@
 <script lang="ts">
 import {defineComponent} from "@vue/runtime-core";
 import {ref} from "vue";
-import {buildTextItemList} from "./__mocks__/list";
-import {QT_UL_ITEM_TYPE_TEXT} from "./item/type";
+import {buildImageItemList, buildTextItemList} from "./__mocks__/list";
+import {QT_UL_ITEM_TYPE_IMAGE, QT_UL_ITEM_TYPE_TEXT} from "./item/type";
 import qt_ul_item_text from './item/text/qt-ul-item-text.vue'
+import qt_ul_item_image from "./item/image/qt-ul-item-image.vue";
 import {QTULTextItem} from "./item/text/QTULItemTextItem";
+import {getRandomInt} from "./__mocks__/random";
 
 export default defineComponent({
-  name: '文字',
+  name: '类型',
   emits: [],
   components: {
-    'qt-ul-item-text': qt_ul_item_text
+    'qt-ul-item-text': qt_ul_item_text,
+    'qt-ul-item-image': qt_ul_item_image
   },
   setup(props, context) {
     let itemList = ref<Array<QTULTextItem>>([])
 
     function onESCreate() {
-      const list = buildTextItemList(QT_UL_ITEM_TYPE_TEXT, 100)
-      console.log('------buildTextItemList-------->>>', list)
-      itemList.value = list
+      const textList = buildTextItemList(QT_UL_ITEM_TYPE_TEXT, 100)
+      const imageList = buildImageItemList(QT_UL_ITEM_TYPE_IMAGE, 100)
+
+      const dataList = []
+
+      for (let i = 0; i < 100; i++) {
+        const index = getRandomInt(0, 100)
+        dataList.push(index % 2 ? textList[i] : imageList[i])
+      }
+
+      console.log('------buildTextItemList-------->>>', dataList)
+      itemList.value = dataList
     }
 
     return {
