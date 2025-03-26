60e4a2372ae8676e3ba431ea3b23665012b230d4
liulipeng <liulipeng@huan.tv>
2025-03-26 18:20:15 +0800

    qt ul sample

--- src/components/qt-ul/qt-ul-item-list-page.vue
+++ src/components/qt-ul/qt-ul-item-list-page.vue
@@ -1,30 +1,27 @@
 <template>
   <div class="es-sdk-root-css" :clipChildren="false">
-    <s-title-view class="es-sdk-content-title-css" :text="this.$options.name"/>
-    <qt-view class="es-sdk-content-divider-css"/>
+    <s-title-view class="es-sdk-content-title-css" :text="this.$options.name" />
+    <qt-view class="es-sdk-content-divider-css" />
     <qt-view class="qt-ul-root-page">
       <qt-ul
-          class="qt-ul-class" ref="ulRef" name="ul"
-          :items="itemList"
-          :spanCount="1920"
-          :enablePlaceholder="false">
-        <template #item="{item}">
-
+        class="qt-ul-class"
+        ref="ulRef"
+        name="ul"
+        :items="itemList"
+        :spanCount="1920"
+        :enablePlaceholder="false"
+      >
+        <template #item="{ item }">
           <qt-ul-item-text
-              :text="item.text"
-              :backgroundColor="item.backgroundColor"
-              v-if="item.type == 3"
-              :focusable="true"
-              :enableFocusBorder="true"/>
+            :text="item.text"
+            v-if="item.type == 3"
+            :focusable="true"
+            :enableFocusBorder="true"
+          />
 
-          <qt-ul-item-list
-              :itemList="item.itemList"
-              v-if="item.type == 6"/>
+          <qt-ul-item-list :itemList="item.itemList" v-if="item.type == 6" />
 
-          <qt-ul-item-image
-              :src="item.url"
-              v-if="item.type == 2"/>
-
+          <qt-ul-item-image :src="item.url" v-if="item.type == 2" />
         </template>
       </qt-ul>
     </qt-view>
@@ -32,13 +29,13 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "@vue/runtime-core";
-import {ref} from "vue";
+import { defineComponent } from '@vue/runtime-core'
+import { ref } from 'vue'
 import qt_ul_item_text from './item/text/qt-ul-item-text.vue'
 import qt_ul_item_list from './item/list/qt-ul-item-list.vue'
-import {QTListViewItem} from "@quicktvui/quicktvui3";
-import qt_ul_item_image from "./item/image/qt-ul-item-image.vue";
-import {buildWaterfallHorizontalListItemList} from "./__mocks__/waterfall";
+import { QTListViewItem } from '@quicktvui/quicktvui3'
+import qt_ul_item_image from './item/image/qt-ul-item-image.vue'
+import { buildWaterfallHorizontalListItemList } from './__mocks__/waterfall'
 
 export default defineComponent({
   name: '一行滚动',
@@ -46,7 +43,7 @@ export default defineComponent({
   components: {
     'qt-ul-item-text': qt_ul_item_text,
     'qt-ul-item-list': qt_ul_item_list,
-    'qt-ul-item-image': qt_ul_item_image
+    'qt-ul-item-image': qt_ul_item_image,
   },
   setup(props, context) {
     const itemList = ref<Array<QTListViewItem>>([])
@@ -61,9 +58,8 @@ export default defineComponent({
       itemList,
       onESCreate,
     }
-  }
-});
-
+  },
+})
 </script>
 
 <style scoped>
@@ -80,5 +76,4 @@ export default defineComponent({
   height: 1080px;
   background-color: darkgray;
 }
-
 </style>
