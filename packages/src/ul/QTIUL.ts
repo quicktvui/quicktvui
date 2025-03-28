c34a02a64f7a602d48f81793b15ae023ae0b6261
liulipeng <liulipeng@huan.tv>
2025-03-28 14:56:32 +0800

    QTIUL

--- packages/src/ul/QTIUL.ts
+++ packages/src/ul/QTIUL.ts
@@ -1,9 +1,8 @@
-import {QTIView} from "../view/QTIView";
-import {QTULInitPosition} from "./QTULInitPosition";
-import {QTPosition} from "../core/QTPosition";
+import { QTULInitPosition } from './QTULInitPosition'
+import { QTPosition } from '../core/QTPosition'
+import { QTIListView } from '../list-view/core/QTIListView'
 
-export interface QTIUL extends QTIView {
-
+export interface QTIUL extends QTIListView {
   scrollToIndex(x: number, y: number, animated: boolean, duration: number, offset: number): void
 
   setInitPosition(initPosition: QTULInitPosition): void
