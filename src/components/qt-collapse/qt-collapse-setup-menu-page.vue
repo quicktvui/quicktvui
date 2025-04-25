50019b835e038dc9604f7021cf606b784fdd5264
liulipeng <liulipeng@huan.tv>
2025-04-25 14:04:08 +0800

    collapse

--- src/components/qt-collapse/qt-collapse-setup-menu-page.vue
+++ src/components/qt-collapse/qt-collapse-setup-menu-page.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="es-sdk-root-css" :clipChildren="false" :clipPadding="false">
-    <s-title-view class="es-sdk-content-title-css" :text="this.$options.name" />
+    <s-title-view class="es-sdk-content-title-css" text="菜单示例 setup" />
     <div class="es-sdk-content-divider-css" />
     <qt-collapse
       ref="collapseRef"
@@ -16,101 +16,87 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { ref } from 'vue'
-import { defineComponent } from '@vue/runtime-core'
 
-import QTCollapseSetupMenuItemOne from './menu/qt-collapse-setup-menu-item-one.vue'
-import QTCollapseSetupMenuItemTwo from './menu/qt-collapse-setup-menu-item-two.vue'
-import QTCollapseSetupMenuItemThree from './menu/qt-collapse-setup-menu-item-three.vue'
-import QTCollapseSetupMenuItemFour from './menu/qt-collapse-setup-menu-item-four.vue'
-import QTCollapseSetupMenuItemFive from './menu/qt-collapse-setup-menu-item-five.vue'
+import QtCollapseSetupMenuItemOne from './menu/qt-collapse-setup-menu-item-one.vue'
+import QtCollapseSetupMenuItemTwo from './menu/qt-collapse-setup-menu-item-two.vue'
+import QtCollapseSetupMenuItemThree from './menu/qt-collapse-setup-menu-item-three.vue'
+import QtCollapseSetupMenuItemFour from './menu/qt-collapse-setup-menu-item-four.vue'
+import QtCollapseSetupMenuItemFive from './menu/qt-collapse-setup-menu-item-five.vue'
 
 import { ESKeyCode, ESKeyEvent, ESLogLevel, useESLog } from '@extscreen/es3-core'
 import { QTCollapse, QTCollapseItem, QTICollapse } from '@quicktvui/quicktvui3'
 
 const TAG = 'QTCollapsePage'
 
-export default defineComponent({
-  name: '菜单示例 setup',
-  emits: [],
-  components: {
-    'qt-collapse-setup-menu-item-one': QTCollapseSetupMenuItemOne,
-    'qt-collapse-setup-menu-item-two': QTCollapseSetupMenuItemTwo,
-    'qt-collapse-setup-menu-item-three': QTCollapseSetupMenuItemThree,
-    'qt-collapse-setup-menu-item-four': QTCollapseSetupMenuItemFour,
-    'qt-collapse-setup-menu-item-five': QTCollapseSetupMenuItemFive,
-  },
-  setup(props, context) {
-    const collapseRef = ref<QTICollapse>()
-    const log = useESLog()
+const collapseRef = ref<QTICollapse>()
+const log = useESLog()
 
-    let menuIndex = -1
+let menuIndex = -1
 
-    function onESCreate() {
-      const collapse: QTCollapse = {
-        width: 1920,
-        height: 800,
-        defaultIndex: 0, //默认0
-        expandDuration: 200, //默认200
-        itemList: [
-          {
-            height: 200,
-            collapseHeight: 150,
-          },
-          {
-            height: 200,
-            collapseHeight: 150,
-          },
-          {
-            height: 200,
-            collapseHeight: 150,
-          },
-          {
-            height: 200,
-            collapseHeight: 150,
-          },
-          {
-            height: 200,
-            collapseHeight: 150,
-          },
-        ],
-      }
-      collapseRef.value?.init(collapse)
+function onESCreate() {
+  const collapse: QTCollapse = {
+    width: 1920,
+    height: 800,
+    defaultIndex: 0, //默认0
+    expandDuration: 200, //默认200
+    itemList: [
+      {
+        height: 200,
+        collapseHeight: 150,
+      },
+      {
+        height: 200,
+        collapseHeight: 150,
+      },
+      {
+        height: 200,
+        collapseHeight: 150,
+      },
+      {
+        height: 200,
+        collapseHeight: 150,
+      },
+      {
+        height: 200,
+        collapseHeight: 150,
+      },
+    ],
+  }
+  collapseRef.value?.init(collapse)
+}
+
+function onCollapseItemExpand(index: number, item: QTCollapseItem) {
+  if (log.isLoggable(ESLogLevel.DEBUG)) {
+    log.d(TAG, '-------onCollapseItemExpand------->>>>', item, index)
+  }
+}
+
+function onKeyDown(keyEvent: ESKeyEvent) {
+  if (keyEvent.keyCode == ESKeyCode.ES_KEYCODE_DPAD_DOWN) {
+    menuIndex++
+    if (menuIndex > 4) {
+      menuIndex = 4
     }
-
-    function onCollapseItemExpand(index: number, item: QTCollapseItem) {
-      if (log.isLoggable(ESLogLevel.DEBUG)) {
-        log.d(TAG, '-------onCollapseItemExpand------->>>>', item, index)
-      }
+    collapseRef.value?.expandItem(menuIndex)
+  } else if (keyEvent.keyCode == ESKeyCode.ES_KEYCODE_DPAD_UP) {
+    menuIndex--
+    if (menuIndex < 0) {
+      menuIndex = 0
     }
+    collapseRef.value?.expandItem(menuIndex)
+  }
+}
 
-    function onKeyDown(keyEvent: ESKeyEvent) {
-      if (keyEvent.keyCode == ESKeyCode.ES_KEYCODE_DPAD_DOWN) {
-        menuIndex++
-        if (menuIndex > 4) {
-          menuIndex = 4
-        }
-        collapseRef.value?.expandItem(menuIndex)
-      } else if (keyEvent.keyCode == ESKeyCode.ES_KEYCODE_DPAD_UP) {
-        menuIndex--
-        if (menuIndex < 0) {
-          menuIndex = 0
-        }
-        collapseRef.value?.expandItem(menuIndex)
-      }
-    }
+function onKeyUp(keyEvent: ESKeyEvent) {}
 
-    function onKeyUp(keyEvent: ESKeyEvent) {}
-
-    return {
-      collapseRef,
-      onESCreate,
-      onCollapseItemExpand,
-      onKeyDown,
-      onKeyUp,
-    }
-  },
+defineExpose({
+  collapseRef,
+  onESCreate,
+  onCollapseItemExpand,
+  onKeyDown,
+  onKeyUp,
 })
 </script>
 
