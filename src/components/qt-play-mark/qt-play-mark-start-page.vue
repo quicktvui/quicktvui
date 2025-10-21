20034f736ddf0d7a250e9f776bf1dec12cb71b6c
liulipeng <pengliliu8086@163.com>
2025-10-21 18:12:54 +0800

    play-mark

--- src/components/qt-play-mark/qt-play-mark-start-page.vue
+++ src/components/qt-play-mark/qt-play-mark-start-page.vue
@@ -3,7 +3,17 @@
     <s-title-view class="es-sdk-content-title-css" :text="this.$options.name" />
     <div class="es-sdk-content-divider-css" />
     <qt-row class="qt-sdk-content-row-css">
-      <play-mark class="qt-sdk-play-mark-css" text="Item"></play-mark>
+      <play-mark class="qt-sdk-play-mark-css" />
+      <play-mark :roundCorner="10" class="qt-sdk-play-mark-css" />
+      <play-mark :gap="10" class="qt-sdk-play-mark-css" />
+      <play-mark :showType="1" class="qt-sdk-play-mark-css" />
+      <play-mark startColor="#FF8534" endColor="#FFCA37" class="qt-sdk-play-mark-css" />
+      <play-mark
+        startColor="#FF8534"
+        endColor="#FFCA37"
+        :isSupportGradient="true"
+        class="qt-sdk-play-mark-css"
+      />
     </qt-row>
   </div>
 </template>
@@ -26,8 +36,9 @@ export default defineComponent({
 }
 
 .qt-sdk-play-mark-css {
-  width: 100px;
-  height: 100px;
+  width: 200px;
+  height: 200px;
   background-color: #40b883;
+  margin: 20px;
 }
 </style>
