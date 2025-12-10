06165ff3d7a12de27b7e1114a292521aadc50ae8
liulipeng <pengliliu8086@163.com>
2025-12-10 16:42:31 +0800

    build tools

--- src/App.vue
+++ src/App.vue
@@ -27,20 +27,20 @@ export default defineComponent({
 
     function onESCreate(app: ESApp, params: ESAppParams) {
       log.setMinimumLoggingLevel(ESLogLevel.DEBUG)
-      return Promise.resolve().then(() => {
-        const playerDisplay: ESPlayerDisplay = {
-          screenWidth: device.getScreenWidth(),
-          screenHeight: device.getScreenHeight(),
-        }
-        const config: ESPlayerConfiguration = {
-          debug: true,
-          display: playerDisplay,
-          device: {
-            deviceType: runtime.getRuntimeDeviceType() ?? '',
-          },
-        }
-        return playerManager.init(config)
-      })
+      // return Promise.resolve().then(() => {
+      //   const playerDisplay: ESPlayerDisplay = {
+      //     screenWidth: device.getScreenWidth(),
+      //     screenHeight: device.getScreenHeight(),
+      //   }
+      //   const config: ESPlayerConfiguration = {
+      //     debug: true,
+      //     display: playerDisplay,
+      //     device: {
+      //       deviceType: runtime.getRuntimeDeviceType() ?? '',
+      //     },
+      //   }
+      //   return playerManager.init(config)
+      // })
     }
 
     function onESCreated(success: boolean) {
