ee9327363b5390c57527e52eb5f2d2f0d261de84
yangsong <yangsongsong@huan.tv>
2026-02-11 17:44:48 +0800

    feat: 长图组件HDR调试

--- src/components/qt-image/qt-image-local-page.vue
+++ src/components/qt-image/qt-image-local-page.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="es-sdk-root-css" :clipChildren="false">
-    <s-title-view class="es-sdk-content-title-css" :text="this.$options.name" />
-    <qt-view class="es-sdk-content-divider-css" />
+    <!--    <s-title-view class="es-sdk-content-title-css" :text="this.$options.name" />-->
+    <!--    <qt-view class="es-sdk-content-divider-css" />-->
     <qt-view class="qt-image-start-page">
-      <qt-image
+      <img
         src="file:///sdcard/Android/data/tv.huan.xiaoyoucast/files/es_files/images/tmp_696f5c30503f5f5435f071cbaa1a8d0e.jpg"
         class="qt-local-image"
       />
@@ -39,5 +39,6 @@ export default defineComponent({
 .qt-image-start-page .qt-local-image {
   width: 1920px;
   height: 1080px;
+  resize-mode: contain;
 }
 </style>
--- src/components/qt-long-image/qt-long-image-start-page.vue
+++ src/components/qt-long-image/qt-long-image-start-page.vue
@@ -8,6 +8,7 @@
         :scaleType="2"
         @onInitializeSuccess="onInitializeSuccess"
         @onInitializeError="onInitializeError"
+        @onImageLoad="onImageLoad"
         class="qt-long-image-root-css"
       />
     </qt-view>
@@ -38,16 +39,17 @@
         @onButtonFocused="onButtonFocused4"
         @onButtonClicked="onButtonClicked(4)"
       />
-      <s-text-button
-        text="色温"
-        @onButtonFocused="onButtonFocused5"
-        @onButtonClicked="onButtonClicked(5)"
-      />
+      <!--      <s-text-button-->
+      <!--        text="色温"-->
+      <!--        @onButtonFocused="onButtonFocused5"-->
+      <!--        @onButtonClicked="onButtonClicked(5)"-->
+      <!--      />-->
       <s-text-button
         text="双层"
         @onButtonFocused="onButtonFocused6"
         @onButtonClicked="onButtonClicked(6)"
       />
+      <s-text-button text="切换图片" @onButtonFocused="onButtonFocused7" />
     </div>
   </qt-view>
 </template>
@@ -55,9 +57,14 @@
 <script lang="ts">
 import { defineComponent } from '@vue/runtime-core'
 import { ref } from 'vue'
-import { QTILongImage, QTLongImageDownloadChangeBean } from '@quicktvui/quicktvui3'
+import {
+  QTILongImage,
+  QTLongImageDownloadChangeBean,
+  QTLongImageLoadStatusChangeBean,
+} from '@quicktvui/quicktvui3'
 import { useESRouter } from '@extscreen/es3-router'
 import { ESKeyCode, ESKeyEvent, useESToast } from '@extscreen/es3-core'
+import { EventBus } from '@extscreen/es3-vue'
 
 export default defineComponent({
   name: '使用初探',
@@ -70,13 +77,16 @@ export default defineComponent({
     const toast = useESToast()
 
     const enableHDR = ref<boolean>(true)
-    const saturation = ref<number>(1.15)
-    const contrast = ref<number>(1.1)
-    const brightness = ref<number>(20)
+    const saturation = ref<number>(1.5)
+    const contrast = ref<number>(1.6)
+    const brightness = ref<number>(10)
     const warmth = ref<number>(1.0)
     const alpha = ref<number>(0)
+    const scale = ref<number>(0)
 
     const current = ref<number>(-1)
+    const currentI = ref<number>(0)
+    let imgs: Array<string> = []
 
     hdrText.value =
       ' enableHDR: ' +
@@ -110,12 +120,27 @@ export default defineComponent({
 
     function onInitializeSuccess() {
       console.log('----------onInitializeSuccess--------->>>>')
-      viewRef.value?.setSrc(
-        // 'https://ss-mpvolc.meipian.me/users/1465746/3abb8b5805d6491f89af4d7e4da94f1bsoi__png.heic~tplv-s1ctq42ewb-s2-cC-q:682:10000:0:0:q80.jpg'
-        // 'file:///storage/emulate/0/Download/Ceremonial4_8k.jpg'
-        // 'file:///storage/emulate/0/test.jpg'
-        'file:///sdcard/Android/data/tv.huan.xiaoyoucast/files/es_files/images/tmp_696f5c30503f5f5435f071cbaa1a8d0e.jpg'
-      )
+
+      // onButtonClicked(1)
+      // onButtonClicked(2)
+      // onButtonClicked(3)
+      // onButtonClicked(4)
+      // onButtonClicked(5)
+      // onButtonClicked(6)
+
+      EventBus.$on('OnLongImageList', (event: Array<string>) => {
+        toast.showLongToast('OnLongImageList ' + event.length)
+        imgs = event
+
+        viewRef.value?.setSrc(
+          'file://' + imgs[0]
+          // 'https://ss-mpvolc.meipian.me/users/1465746/3abb8b5805d6491f89af4d7e4da94f1bsoi__png.heic~tplv-s1ctq42ewb-s2-cC-q:682:10000:0:0:q80.jpg'
+          // 'file:///storage/emulate/0/Download/Ceremonial4_8k.jpg'
+          // 'file:///storage/emulate/0/test.jpg'
+          // 'file:///sdcard/Android/data/tv.huan.xiaoyoucast/files/es_files/images/tmp_696f5c30503f5f5435f071cbaa1a8d0e.jpg'
+          // 'file:///sdcard/Android/data/tv.huan.xiaoyoucast/files/es_files/images/tmp_034b312491bf31cde56ae886812c1549.jpg'
+        )
+      })
     }
 
     function onInitializeError(error) {
@@ -130,35 +155,42 @@ export default defineComponent({
       switch (index) {
         case 1:
           viewRef.value?.isHDR().then((res) => {
-            toast.showLongToast('isHDR ' + res)
+            // toast.showLongToast('isHDR ' + res)
+            enableHDR.value = res
           })
           break
         case 2:
           viewRef.value?.getSaturation().then((res) => {
-            toast.showLongToast('getSaturation ' + res)
+            // toast.showLongToast('getSaturation ' + res)
+            saturation.value = res
           })
           break
         case 3:
           viewRef.value?.getContrast().then((res) => {
-            toast.showLongToast('getContrast ' + res)
+            // toast.showLongToast('getContrast ' + res)
+            contrast.value = res
           })
           break
         case 4:
           viewRef.value?.getBrightness().then((res) => {
-            toast.showLongToast('getBrightness ' + res)
+            // toast.showLongToast('getBrightness ' + res)
+            brightness.value = res
           })
           break
         case 5:
           viewRef.value?.getWarmth().then((res) => {
-            toast.showLongToast('getWarmth ' + res)
+            // toast.showLongToast('getWarmth ' + res)
+            warmth.value = res
           })
           break
         case 6:
           viewRef.value?.getOverlayAlpha().then((res) => {
-            toast.showLongToast('getOverlayAlpha ' + res)
+            // toast.showLongToast('getOverlayAlpha ' + res)
+            alpha.value = res
           })
           break
       }
+      updateHDRText()
     }
 
     function onButtonFocused1(focused) {
@@ -178,6 +210,10 @@ export default defineComponent({
     }
     function onButtonFocused6(focused) {
       if (focused) current.value = 6
+    }
+
+    function onButtonFocused7(focused) {
+      if (focused) current.value = 7
     }
 
     function onKeyDown(keyEvent: ESKeyEvent) {
@@ -206,6 +242,10 @@ export default defineComponent({
           case 6:
             alpha.value = alpha.value - 10
             viewRef.value?.setOverlayAlpha(alpha.value)
+            break
+          case 7:
+            currentI.value = (currentI.value + 1) % imgs.length
+            viewRef.value?.setSrc('file://' + imgs[currentI.value])
             break
         }
         updateHDRText()
@@ -253,7 +293,21 @@ export default defineComponent({
         ' 色温:' +
         warmth.value +
         ' overAlpha:' +
-        alpha.value
+        alpha.value +
+        ' scale:' +
+        scale.value
+    }
+
+    function onImageLoad(changeBean: QTLongImageLoadStatusChangeBean) {
+      qt.log.e('ok->onImageLoad', JSON.stringify(changeBean))
+      switch (changeBean.status) {
+        case 1: // 加载成功
+          scale.value = Number(changeBean.scale)
+          break
+        case 2: // 加载失败
+          qt.toast.showToast('暂不支持该文件类型')
+          break
+      }
     }
 
     return {
@@ -270,7 +324,9 @@ export default defineComponent({
       onButtonFocused4,
       onButtonFocused5,
       onButtonFocused6,
+      onButtonFocused7,
       onKeyDown,
+      onImageLoad,
     }
   },
 })
